903c4ec98eece9d5a1f75c7fa2b119adae16aaf2
Rasool <rasoolol.77@gmail.com>
2025-08-31 21:16:14 +0330

    feat: update signin and signup pages

--- src/auth/views/SignIn.view.vue
+++ src/auth/views/SignIn.view.vue
@@ -1,4 +1,71 @@
 <template>
-  <h3>SignIn.view.vue</h3>
-  <RouterLink :to="{ name: 'signup' }">SignUp</RouterLink>
+  <div class="signin">
+    <form class="signin__form" @submit.prevent="handleSignin">
+      <CustomInput
+        label="Username or Email"
+        name="identifier"
+        rules="required"
+        v-model="data.identifier"
+      />
+      <CustomInput
+        label="Password"
+        name="password"
+        rules="required|min:5"
+        type="password"
+        v-model="data.password"
+      />
+      <div class="flex gap-2">
+        <input type="checkbox" v-model="data.remember" id="remember" />
+        <label for="remember">remember me</label>
+      </div>
+      <CustomButton type="submit">ورود</CustomButton>
+    </form>
+    <div>
+      don't have account?
+      <RouterLink :to="{ name: 'signup' }">SignUp</RouterLink>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { useAuthStore } from '@/auth/stores';
+const authStore = useAuthStore();
+
+//TODO use vee-validate, vue-query, error handling and fixing ui when I have time :)
+// const { handleSubmit, resetField } = useForm();
+// const authStore = useAuthStore();
+
+// const handleLogin = handleSubmit(async (formData) => {
+//   try {
+//     await authStore.login({ formData });
+//     router.replace({ name: "dashboard" });
+//   } catch (error) {
+//     console.error("An error occurred:", error);
+//   } finally {
+//   }
+// });
+
+const data = reactive({
+  identifier: '',
+  password: '',
+  remember: false,
+});
+const handleSignin = () => {
+  authStore.login(data);
+};
+</script>
+
+<style lang="scss" scoped>
+.signin {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  &__form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+}
+</style>
--- src/auth/views/SignUp.view.vue
+++ src/auth/views/SignUp.view.vue
@@ -1,4 +1,74 @@
 <template>
-  <h3>SignUp.view.vue</h3>
-  <RouterLink :to="{ name: 'signin' }">SignIn</RouterLink>
+  <div class="signup">
+    <form class="signup__form" @submit.prevent="handleSignup">
+      <CustomInput
+        label="Username"
+        name="username"
+        rules="required"
+        v-model="data.username"
+      />
+      <CustomInput
+        label="Email"
+        name="email"
+        rules="required"
+        v-model="data.email"
+      />
+      <CustomInput
+        label="Password"
+        name="password"
+        rules="required|min:5"
+        type="password"
+        v-model="data.password"
+      />
+      <CustomInput
+        label="Password Confirmation"
+        name="confirmPassword"
+        rules="required|min:5"
+        type="password"
+        v-model="data.confirmPassword"
+      />
+      <div class="flex gap-2">
+        <input type="checkbox" v-model="data.terms" id="terms" />
+        <label for="terms">accept the terms</label>
+      </div>
+      <CustomButton type="submit">register</CustomButton>
+    </form>
+    <div>
+      already have account?
+      <RouterLink :to="{ name: 'signin' }">SignIn</RouterLink>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { useAuthStore } from '@/auth/stores';
+const authStore = useAuthStore();
+
+//TODO use vee-validate, vue-query, error handling and fixing ui when I have time :)
+
+const data = reactive({
+  username: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  terms: false,
+});
+const handleSignup = () => {
+  authStore.register(data);
+};
+</script>
+
+<style lang="scss" scoped>
+.signup {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  &__form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+}
+</style>
