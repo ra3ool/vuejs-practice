1a13013123aea0315c71f01fc56130620c788b48
Rasool <rasoolol.77@gmail.com>
2025-08-31 22:51:26 +0330

    feat: add card component

---
+++ src/shared/components/card.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'cardComponent',
+});
+</script>
+
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  border-radius: var(--c-card-border-radius, 12px);
+  padding: var(--c-card-padding-y, 32px) var(--c-card-padding-x, 32px);
+  background-color: var(--c-card-background-color, var(--color-white));
+  width: 100%;
+  overflow: hidden;
+}
+</style>
--- src/shared/layouts/default.layout.vue
+++ src/shared/layouts/default.layout.vue
@@ -12,6 +12,8 @@ defineOptions({
 
 <style lang="scss" scoped>
 .default-layout {
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
 }
